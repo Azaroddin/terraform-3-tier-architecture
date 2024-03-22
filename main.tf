c520d62376f47309f9522ac5f3801256dea9cd1b
Azaroddin <158464687+Azaroddin@users.noreply.github.com>
2024-03-22 21:28:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -188,7 +188,7 @@ resource "aws_instance" "appserver2" {
   }
 }
 
-resource "aws_db_instance_k2" "default_k2" {
+resource "aws_db_instance" "default" {
   allocated_storage      = 10
   db_subnet_group_name   = aws_db_subnet_group.default.id
   engine                 = "mysql"
